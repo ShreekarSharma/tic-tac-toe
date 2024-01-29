74ba0ea156e5b421c5228166960a68c28cdba918
Shreekar Sharma <shreekar@mail.com>
2024-01-29 14:45:01 +0530

    initial commit

---
+++ src/app/components/icon/icon.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { faC, faPen, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.css'
+})
+export class IconComponent implements OnInit {
+  @Input() iconName: string = '';
+  faPen = faPen;
+  faTimes = faTimes;
+  faCircle = faCircle;
+  constructor(){}
+  ngOnInit():void{}
+}
